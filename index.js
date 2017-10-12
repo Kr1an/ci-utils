39f0a0fc1176be7a4d18d3c4b4cd8c24072a3549
Anton Krinitsyn <anton.krinitsyn@codex-soft.com>
2017-10-12 16:23:20 +0300

    ci-utils/feature: addd run command

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 var http = require('http');
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var kill = require('cross-port-killer').kill;
 
 http.createServer((req, res) => {
-	exec("fuser -k -n tcp " + process.env.APP_PORT || 3000);
-	exec("cd " + process.env.APP_PATH + " && " + process.env.RUN_COMMAND || "npm run start:production");
-	res.end("ended");
+	kill(process.env.APP_PORT || 3000).then((dip) => {
+		console.log('killed process with dip: ' + dip);
+		startProduction(process.env.APP_PATH, process.env.NPM_COMMAND || 'start:production');
+
+	})
+
+	// var child = spawn('npm', ['run', 'start:production-no-tests'], {cwd: '/home/anton/Workspace/empiresolomonbank'});
+	// child.stderr.on('data', stderr);
+	// child.stdout.on('data', stdout);
+
+	res.end('_');
+	
 }).listen(process.env.UTILS_PORT || 3001);
+
+var startProduction = function(path, npmCommand) {
+	ioToConsoleProcess(spawn('npm', ['run', npmCommand], {cwd: path}));
+}
+
+var ioToConsoleProcess = function(process) {
+	process.stderr.on('data', stderr);
+	process.stdout.on('data', stdout);
+	process.on('exit', onExit);
+	return process;
+}
+var onExit = function() {
+	console.log("Process endedup")
+}
+var stdout = function(data) {
+	console.log(data.toString());
+}
+var stderr = function(data) {
+	console.error(data.toString());
+}
